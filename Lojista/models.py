8153cdb59e4825886a132e7bb0e612812aa07330
yanzeradev <107873465+yanzeradev@users.noreply.github.com>
2025-01-31 16:58:51 -0300

    Update models.py

--- Lojista/models.py
+++ Lojista/models.py
@@ -5,14 +5,14 @@ import cv2
 
 def load_models():
     # Carregar os modelos YOLO
-    model_detection = YOLO("yolov8n.pt")
-    model_classification = YOLO(r"C:\Users\yanka\Documents\DEV\YanProjeto\projeto_cam_ufcat\modelo_treinado_v11_SGD-v4.pt")
+    model_detection = YOLO("yolov8n-seg.pt")
+    model_classification = YOLO(r"C:\Users\yanka\Documents\DEV\SenseVision\projeto_cam_ufcat\modelo_treinado_v8_Adam-310125-64batch.pt")
 
     # Inicializar o extrator de características do Torchreid
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     extractor = torchreid.utils.FeatureExtractor(
         model_name='osnet_x1_0',
-        model_path=r'C:\Users\yanka\Documents\DEV\YanProjeto\osnet_x1_0_imagenet.pth',
+        model_path=r'C:\Users\yanka\Documents\DEV\SenseVision\projeto_cam_ufcat\osnet_x1_0_imagenet.pth',
         device=device
     )
     return model_detection, model_classification, extractor
