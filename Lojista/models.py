0961ccff7ee8ea1857ed17b3e65898c909f1b10e
yanzeradev <107873465+yanzeradev@users.noreply.github.com>
2025-03-29 13:56:13 -0300

    Update models.py

--- Lojista/models.py
+++ Lojista/models.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
-import torchreid
 import torch
 import cv2
 import logging
+from deep_sort_realtime.deepsort_tracker import DeepSort
 
 # Configurar logging
 logging.basicConfig(level=logging.INFO)
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 def load_models():
     """
-    Carrega os modelos YOLO para detecção e classificação, e o extrator de características OSNet.
+    Carrega os modelos YOLO para detecção e classificação, e o tracker DeepSORT.
     """
     # Verificar dispositivo (CPU ou GPU)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -23,13 +23,14 @@ def load_models():
     logger.info("Carregando modelo YOLO para classificação...")
     model_classification = YOLO(r"projeto_cam_ufcat\modelo_treinado_v8_Adam-310125-64batch.pt").to(device)
 
-    # Inicializar o extrator de características do Torchreid (OSNet)
-    logger.info("Carregando extrator de características OSNet...")
-    extractor = torchreid.utils.FeatureExtractor(
-        model_name='osnet_x1_0',
-        model_path=r'projeto_cam_ufcat\osnet_x1_0_imagenet.pth',
-        device=device
+    # Inicializar o tracker DeepSORT
+    logger.info("Inicializando tracker DeepSORT...")
+    tracker = DeepSort(
+        max_age=50,            # Quantos frames um objeto é mantido antes de ser esquecido
+        n_init=20,              # Frames necessários para confirmar um objeto
+        nms_max_overlap=0.5,   # Usa GPU para extração de características
+        nn_budget=100          # Memória do modelo de ReID
     )
 
     logger.info("Modelos carregados com sucesso!")
-    return model_detection, model_classification, extractor
+    return model_detection, model_classification, tracker
